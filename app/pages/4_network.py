a2c03c1d970196902241909e73f7d8c0c54abdf6
Wizen <guzmanpjaime@gmail.com>
2023-05-07 10:30:33 -0600

    Added visualizations and fixed some error in the network page

--- app/pages/4_network.py
+++ app/pages/4_network.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import IsolationForest
 import altair as alt
 from sklearn.neighbors import KernelDensity
 import sranodec as anom
+import numpy as np
 
 st.set_page_config(
     page_title="Network Monitoring",
@@ -20,28 +21,32 @@ st.markdown('To standarize data among all nodes we decided to change in and out 
 st.title("Network Monitoring")
 
 with st.spinner("Cargando, espera..."):
-    network = pd.read_pickle("app/data/network.pkl")
+    network = pd.read_pickle("../data/network.pkl")
     network["timestamp"] = network["timestamp"].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%dT%H:%M:%S.%fZ'))
     network = network.resample('1min', on="timestamp").agg("first").ffill()
     network['in.pct'] = (network['network.in.bytes'] / (network['network.in.bytes'] + network['network.out.bytes']))*100
     network['out.pct'] = (network['network.out.bytes'] / (network['network.in.bytes'] + network['network.out.bytes']))*100
+    network['timestamp'] = network.index
+    network = network.dropna()
 
+st.line_chart(network['in.pct'])
 
+st.dataframe(network)
 st.subheader('Exploratory Analysis')
 
 st.markdown('To understand the data we first wrote a function called `plot_network_info`, which shows us the data of each of the nodes in a time series. This was usefull for we were')
 st.markdown('Able to see periodicity and see some clear examples of outliers. After that function came a lineplot comparison where we utilize seabron to visualize the change in time')
 st.markdown('of all the nodes against each other and can start to get a sense of wheter it\'s related or not.' )
 
-c = alt.Chart(network, title='Comparison of each node over time').mark_line.encode(
+c = alt.Chart(network).mark_line().encode(
     x='timestamp',
     y='in.pct',
-    c='node:N'
-)
+    color='node:N')
 
 st.altair_chart(c, use_container_width=True)
 
 st.subheader('Anomaly detection')
+
 st.markdown('For anomaly detection we decided to apply a concept that microsoft utilizes with their own servers for anomaly detection. First we imported a libreary since the functions')
 st.markdown('Where already written for us to use. The code base for the function isn\'t to long, but it\'s still better to import than to copy.')
 st.markdown('The function we utilize is based on spectral anomaly detection via use of CNNs. The main role is to change our data utilizing a fourier transform, allowing the CNN to understand')
@@ -50,6 +55,24 @@ st.markdown('set that any value above 99 percent certainty is considered an outl
 st.markdown('returned to us as a score, where the higher the number the more likely it is an anomaly.')
 st.markdown('After seeing that the algorithm worked we then created a function to not only calculate our score for the data point, but also to plot the results and see wheter it looks')
 st.markdown('correct.')
+
+st.markdown('The formula for this algorithm is based on the following sequence for **x**')
+formula_latex = r'''
+$$
+\begin{equation*}
+    \begin{align*}
+        A(f) = \text{Amplitude}((\mathscr{F}(\textrm{x}))) \\
+        P(f) = \text{Phrase}((\mathscr{F}(\textrm{x}))) \\
+        L(f) = log(A(f)) \\
+        AL(f) = h_q(f) \cdot L(f)
+        R(f) = L(f) - AL(f)
+        S(\textrm{x}) = || \mathscr{F}^{-1} (exp(R(f)+iP(f)))||
+    \end{align*}
+\end{equation*}
+$$
+'''
+
+st.markdown(formula_latex)
 
 with st.spinner("Ajustando modelo, espera..."):
     # less than period
@@ -73,29 +96,33 @@ with st.spinner("Ajustando modelo, espera..."):
     score_in = spec.generate_anomaly_score(network['in.pct'])
     score_out = spec.generate_anomaly_score(network['out.pct'])
 
-    c = alt.Chart(network['in.pct'], title="Percentage of incoming traffic").mark_line().encode(
-    x='timestamp',
-    y='in.pct')
-    
-    d = alt.Chart(network['out.pct'], title="Percentage of outgoing traffic").mark_line().econde(
-        x='timestamp',
-        y='out.pct'
-
     index_changes_in = np.where(score_in > np.percentile(score_in, 99))[0]
     index_changes_out = np.where(score_out > np.percentile(score_out, 99))[0]
-    
-    e = alt.Chart(score_in).mark_circle(size=60).encode(
-        x = network['in.pct'].iloc[index_changes_in].index,
-        y = network['in.pct'].iloc[index_changes_in],
-        color='green'
-    )
 
-    st.altair_chart(c+e, use_container_width=True)
 
-    f = alt.Chart(score_out).mark_circle(size=60).encode(
-        x = network['in.pct'].iloc[index_changes_out].index,
-        y = network['in.pct'].iloc[index_changes_out],
-        color='green'
-    )
+c = alt.Chart(network, title="Percentage of incoming traffic").mark_line().encode(
+    x='timestamp',
+    y='in.pct'
+)
 
-    st.altair_chart(d+f, use_container_width=True)
+d = alt.Chart(network, title="Percentage of outgoing traffic").mark_line().encode(
+    x='timestamp',
+    y='out.pct'
+)    
+
+
+e = alt.Chart(network.iloc[index_changes_in]).mark_circle(size=60).encode(
+    x = 'timestamp',
+    y = 'in.pct',
+    color=alt.value('red')
+)
+
+st.altair_chart(c+e, use_container_width=True)
+
+f = alt.Chart(network.iloc[index_changes_out]).mark_circle(size=60).encode(
+    x = 'timestamp',
+    y = 'out.pct',
+    color=alt.value('red')
+)
+
+st.altair_chart(d+f, use_container_width=True)
